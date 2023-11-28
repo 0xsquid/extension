5890ddd7233ebf7fa0e146f9bce955b96314afff
Connor <wardconnor17@gmail.com>
2023-11-28 13:31:00 +0000

    update extension styles

--- dist/service-worker-loader.js
+++ dist/service-worker-loader.js
@@ -1,1 +1,3 @@
-import './assets/serviceWorker.js-4ed993c7.js';
+import 'http://localhost:5173/@vite/env';
+import 'http://localhost:5173/@crx/client-worker';
+import 'http://localhost:5173/serviceWorker.js';
